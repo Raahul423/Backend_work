975dfce1e9190df419ad6fe7e5cc0e4c1dd1a254
Raahul423 <rpal24480@gmail.com>
2025-10-16 16:37:19 +0530

    adding 2 function 1st generate hash password and 2nd one generate refresh token or access token

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -39,27 +39,52 @@ const UserSchema = new mongoose.Schema(
         ref: "Vedio",
       },
     ],
-    refrehToken:{
-        type:String
-    }
+    refrehToken: {
+      type: String,
+    },
   },
   {
     timestamps: true,
   }
 );
 
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-UserSchema.pre("save",async function(next){
-    if (!this.isModified("password")) return next();
+  const salt = await bycrpt.genSalt(10);
+  this.password = await bycrpt.hash(this.password, salt);
+  next();
+});
 
-    const salt = await bycrpt.genSalt(10)
-    this.password = await bycrpt.hash(this.password,salt)
-    next();
-})
+UserSchema.methods.ispasswordcorrect = async function (password) {
+  return await bycrpt.compare(password, this.password);
+};
+
+UserSchema.methods.generaterefreshtoken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
 
 
-UserSchema.methods.ispasswordcorrect=async function(password) {
-    return await bycrpt.compare(password,this.password);
+UserSchema.methods.refrehToken= function(){
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
 }
 
 export const User = mongoose.model("User", UserSchema);
